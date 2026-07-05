0a2a68b61587b7f7ca1e405a04b0ccbfad2ee15d
fayyoz24 <fayyozusmon@gmail.com>
2026-07-05 22:59:24 +0500

    added

--- src/api/cart.js
+++ src/api/cart.js
@@ -2,7 +2,7 @@ import { api } from "./client";
 
 export const cartApi = {
   get: () => api.get("/cart/"),
-  clear: () => api.delete("/cart/"),
+  clear: () => api.post("/cart/clear/"),
   addItem: ({ product_id, qty = 1, variant_id, modifier_option_ids = [], instructions = "" }) =>
     api.post("/cart/items/", { product_id, qty, variant_id, modifier_option_ids, instructions }),
   updateItem: (itemId, qty) => api.patch(`/cart/items/${itemId}/`, { qty }),
